c9aa1dea070b1ee39c2ba0afc7568d36a69f181a
Wojciech Szwaczka <wojciech.szwaczka@wssolutions.net>
2024-02-05 17:20:42 +0100

    Rebuild add crawl component

--- src/components/CrawlerApp/CrawlVisualization/AddCrawlComponent.tsx
+++ src/components/CrawlerApp/CrawlVisualization/AddCrawlComponent.tsx
@@ -1,7 +1,7 @@
-import { Button, Label, Modal, TextInput } from "flowbite-react";
+import { Badge, Button, Label, Modal, TextInput } from "flowbite-react";
 import Crawl from "../../../interfaces/Crawl";
 import { HiOutlineBookmark, HiOutlineStar } from "react-icons/hi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCreateCrawl } from "../../../services/crawlMutations";
 
 type Props = {
@@ -10,28 +10,34 @@ type Props = {
 
 function AddCrawlComponent({data}: Props) {
     const [openModal, setOpenModal] = useState(false);
+    const [showError, setShowError] = useState(false);
+    const [disabledSubmit, setDisabledSubmit] = useState(true);
     const [crawlName, setCrawlName] = useState('');
-    const [error, setError] = useState('');
+
+    useEffect(() => {
+        if(crawlName !== "") {
+            setDisabledSubmit(false);
+        } else {
+            setDisabledSubmit(true);
+        }
+    }, [crawlName]);
+
     const createCrawlMutation = useCreateCrawl();
 
     const onSaveCrawlClick = () => {
-        if(crawlName == '') {
-            setError('Providing crawl name is required to save it!');
-            return;
-        }
-
         data.Name = crawlName;
         createCrawlMutation.mutate(data);
+        setShowError(true);
+    };
 
-        setOpenModal(false);
-        setCrawlName('');
-        setError('')
+    const handleOnChange = (e) => {
+        setCrawlName(e.target.value);
+        setShowError(false);
     };
 
     const onCloseModalClick = () => {
         setOpenModal(false);
         setCrawlName('');
-        setError('');
     };
 
     return (
@@ -39,41 +45,50 @@ function AddCrawlComponent({data}: Props) {
             <Button color='success' size='xl' onClick={() => setOpenModal(true)} className='rounded-full max-h-28 md:rounded-3xl md:max-h-12 md:min-w-max items-center'>
                 <HiOutlineBookmark className='text-2xl' />
             </Button>
-            <Modal show={openModal} size='md' onClose={() => setOpenModal(false)} popup>
+            <Modal show={openModal} size='md' onClose={() => onCloseModalClick()} popup>
                 <Modal.Header />
                 <Modal.Body>
                     <div className='text-center'>
                         <HiOutlineStar className='mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200' />
-                        <div className='text-left'>
-                            <div className='mb-2 block'>
-                                <Label htmlFor='name' value='Name of your Crawl:' />
+                        <div className='flex justify-center'>
+                        { createCrawlMutation.isSuccess ?
+                            <div className='flex flex-col justify-center gap-4'>
+                                <Badge color="success">Success Crawl was correctly added.</Badge>
+                            </div> :
+                            <div>
+                                <div className='text-left'>
+                                    <div className='mb-2 block'>
+                                        <Label htmlFor='name' value='Name of your Crawl:' />
+                                    </div>
+                                    <TextInput
+                                        id='name'
+                                        placeholder='Name'
+                                        value={crawlName}
+                                        onChange={(event) => handleOnChange(event)}
+                                    />
+                                    <div className='h-10 text-center font-semibold text-red-500'>
+                                    {
+                                        createCrawlMutation.isError && showError ?
+                                            <div>
+                                                <span className='font-bold'>Error!</span> {createCrawlMutation.error?.message}
+                                            </div> :
+                                            null
+                                    }
+                                    </div>
+                                </div>
+                                <h3 className='mb-5 text-lg font-normal text-gray-500 dark:text-gray-400'>
+                                    Are you sure you want to save crawl with name: <b>{crawlName}</b>?
+                                </h3>
+                                <div className='flex justify-center gap-4'>
+                                    <Button color='success' onClick={() => onSaveCrawlClick()} disabled={disabledSubmit}>
+                                        Save it!
+                                    </Button>
+                                    <Button color='failure' onClick={() => onCloseModalClick()}>
+                                        No, it was a mistake!
+                                    </Button>
+                                </div>
                             </div>
-                            <TextInput
-                                id='name'
-                                placeholder='Name'
-                                value={crawlName}
-                                onChange={(event) => setCrawlName(event.target.value)}
-                            />
-                            <div className='h-10 text-center font-semibold text-red-500'>
-                            {
-                                error !== '' ?
-                                    <div>
-                                        <span className='font-bold'>Error!</span> {error}
-                                    </div> :
-                                    null
-                            }
-                            </div>
-                        </div>
-                        <h3 className='mb-5 text-lg font-normal text-gray-500 dark:text-gray-400'>
-                            Are you sure you want to save crawl with name: <b>{crawlName}</b>?
-                        </h3>
-                        <div className='flex justify-center gap-4'>
-                            <Button color='success' onClick={() => onSaveCrawlClick()}>
-                                Save it!
-                            </Button>
-                            <Button color='failure' onClick={() => onCloseModalClick()}>
-                                No, it was a mistake!
-                            </Button>
+                        }
                         </div>
                     </div>
                 </Modal.Body>
--- src/services/crawlMutations.ts
+++ src/services/crawlMutations.ts
@@ -1,12 +1,20 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import Crawl from "../interfaces/Crawl";
 import { deleteCrawl, saveCrawl } from "./crawlService";
+import { AxiosError } from "axios";
 
 export function useCreateCrawl() {
     const queryClient = useQueryClient();
 
     return useMutation({
         mutationFn: (data: Crawl) => saveCrawl(data),
+        onError(error, variables) {
+            const axiosError = error as AxiosError;
+
+            if (axiosError.response?.status === 409) {
+                error.message = `The name ${variables.Name} is already in use. Please pick different name.`;
+            }
+        },
         onSettled: async (_, error) => {
             if (error) {
                 console.log(error);
